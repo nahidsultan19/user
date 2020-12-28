574077db348e297aecde15c3d0ddbebf5908b309
nahidsultan19 <nahidsultan38@gmail.com>
2020-12-28 23:45:58 +0600

    member update view link to urls

--- member/urls.py
+++ member/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.MemberView, name='form-page'),
     path('member/', views.MemberList, name='member-list'),
-    path('update/<str:pk>/', views.MemberUpdate, name='member-detail')
+    path('update/<str:pk>/', views.MemberUpdate, name='member-update')
 ]
