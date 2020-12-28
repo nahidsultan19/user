e029b39ac6d2cd716b8576fd9f943d626133e377
nahidsultan19 <nahidsultan38@gmail.com>
2020-12-29 00:05:08 +0600

    add member delete view into urls section

--- member/urls.py
+++ member/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.MemberView, name='form-page'),
     path('member/', views.MemberList, name='member-list'),
-    path('update/<str:pk>/', views.MemberUpdate, name='member-update')
+    path('update/<str:pk>/', views.MemberUpdate, name='member-update'),
+    path('delete/<str:pk>/', views.MemberDelete, name='member-delete')
 ]
