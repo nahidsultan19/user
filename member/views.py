edede65b571e200d3017911cc7aa848f955d0e60
nahidsultan19 <nahidsultan38@gmail.com>
2020-12-29 22:57:34 +0600

    Add Bootstrap message alert

--- member/views.py
+++ member/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+# message module
+from django.contrib import messages
+
 from .models import Member
 from .forms import MemberForm
 
@@ -13,6 +16,7 @@ def MemberView(request):
         form = MemberForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, f'Information Successfully Submitted!')
         return redirect('member-list')
     else:
         members = Member.objects.all()
@@ -33,6 +37,7 @@ def MemberUpdate(request, pk):
         form = MemberForm(request.POST, instance=member)
         if form.is_valid():
             form.save()
+            messages.success(request, f'Member successfully Updated!')
         return redirect('member-list')
     context = {'form': form}
     return render(request, 'member-update.html', context)
@@ -42,6 +47,7 @@ def MemberDelete(request, pk):
     member_delete = Member.objects.get(id=pk)
     if request.method == 'POST':
         member_delete.delete()
+        messages.warning(request, f'Member Deleted!')
         return redirect('member-list')
     context = {'member_delete': member_delete}
     return render(request, 'member-delete.html', context)
