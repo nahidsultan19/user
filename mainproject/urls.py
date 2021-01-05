8b2ae372a0f4991baa1f376f0563fd7c3c288bc0
nahidsultan19 <nahidsultan38@gmail.com>
2021-01-05 15:44:34 +0600

    Add music url

--- mainproject/urls.py
+++ mainproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('member.urls'))
+    path('', include('music.urls')),
+    path('member/', include('member.urls'))
 ]
