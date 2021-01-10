17a1621e55fdc962a74d6a42a4cc669203932fed
nahidsultan19 <nahidsultan38@gmail.com>
2021-01-10 20:55:13 +0600

    create models in music  app

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # app
     'member.apps.MemberConfig',
+    'music.apps.MusicConfig',
     'phone_field',
     'crispy_forms',
 ]
--- music/models.py
+++ music/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+Genre = (
+    ('Rock', 'rock'),
+    ('Metal', 'metal'),
+    ('Folk', 'folk'),
+    ('Pop', 'pop'),
+)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    genre = models.CharField(choices=Genre, max_length=10)
+
+
+class Track(models.Model):
+    title = models.CharField(max_length=100)
+    len = models.IntegerField()
+    rating = models.IntegerField()
+    count = models.IntegerField()
